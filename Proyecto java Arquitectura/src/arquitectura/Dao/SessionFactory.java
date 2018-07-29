42301702ff2383a0dbbac17d63c38c3152631f8a
Raul Ubillos <raulubillos@gmail.com>
2018-07-29 16:07:56 -0300

    version inicial TP

---
+++ Proyecto java Arquitectura/src/arquitectura/Dao/SessionFactory.java
@@ -0,0 +1,8 @@
+package arquitectura.Dao;
+import org.hibernate.*;
+import org.hibernate.boot.registry.*;
+public class SessionFactory {
+	protected void setUp() {
+		final StandardServiceRegistry registry;
+	}
+}
